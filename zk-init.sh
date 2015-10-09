7fd14cd9368263a3b25cf5c9d17c997073ee4a23
Thijs Schnitger <thijs@schnitger.org>
2015-10-09 09:59:27 +0200

    set the path back to /tmp/zookeeper

--- zk-init.sh
+++ zk-init.sh
@@ -4,7 +4,7 @@ ZK=$1
 MYID=1
 IPADDRESS=`ip -4 addr show scope global dev eth0 | grep inet | awk '{print \$2}' | cut -d / -f 1`
 
-cd /zookeeper-3.5.1-alpha
+cd /tmp/zookeeper
 if [ -n "$ZK" ] 
 then
   output=`./bin/zkCli.sh -server $ZK:2181 get /zookeeper/config | grep ^server` 
@@ -23,16 +23,16 @@ then
   
   # get the next increasing number from the sequence
   MYID=$((${sorted_id_list[${#sorted_id_list[@]}-1]}+1))
-  echo $output >> /zookeeper-3.5.1-alpha/conf/zoo.cfg.dynamic
-  echo "server.$MYID=$IPADDRESS:2888:3888:observer;2181" >> /zookeeper-3.5.1-alpha/conf/zoo.cfg.dynamic
-  cp /zookeeper-3.5.1-alpha/conf/zoo.cfg.dynamic /zookeeper-3.5.1-alpha/conf/zoo.cfg.dynamic.org
-  /zookeeper-3.5.1-alpha/bin/zkServer-initialize.sh --force --myid=$MYID
-  ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /zookeeper-3.5.1-alpha/bin/zkServer.sh start
-  /zookeeper-3.5.1-alpha/bin/zkCli.sh -server $ZK:2181 reconfig -add "server.$MYID=$IPADDRESS:2888:3888:participant;2181"
-  /zookeeper-3.5.1-alpha/bin/zkServer.sh stop
-  ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /zookeeper-3.5.1-alpha/bin/zkServer.sh start-foreground
+  echo $output >> /tmp/zookeeper/conf/zoo.cfg.dynamic
+  echo "server.$MYID=$IPADDRESS:2888:3888:observer;2181" >> /tmp/zookeeper/conf/zoo.cfg.dynamic
+  cp /tmp/zookeeper/conf/zoo.cfg.dynamic /tmp/zookeeper/conf/zoo.cfg.dynamic.org
+  /tmp/zookeeper/bin/zkServer-initialize.sh --force --myid=$MYID
+  ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /tmp/zookeeper/bin/zkServer.sh start
+  /tmp/zookeeper/bin/zkCli.sh -server $ZK:2181 reconfig -add "server.$MYID=$IPADDRESS:2888:3888:participant;2181"
+  /tmp/zookeeper/bin/zkServer.sh stop
+  ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /tmp/zookeeper/bin/zkServer.sh start-foreground
 else
-  echo "server.$MYID=$IPADDRESS:2888:3888;2181" >> /zookeeper-3.5.1-alpha/conf/zoo.cfg.dynamic
-  /zookeeper-3.5.1-alpha/bin/zkServer-initialize.sh --force --myid=$MYID
-  ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /zookeeper-3.5.1-alpha/bin/zkServer.sh start-foreground
+  echo "server.$MYID=$IPADDRESS:2888:3888;2181" >> /tmp/zookeeper/conf/zoo.cfg.dynamic
+  /tmp/zookeeper/bin/zkServer-initialize.sh --force --myid=$MYID
+  ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /tmp/zookeeper/bin/zkServer.sh start-foreground
 fi
