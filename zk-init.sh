2a8781dc52a4798fc1c00e8b7e2b09f44f976233
ccrain <charles.crain@kukanao.com>
2015-11-13 22:28:51 -0600

    This now appears to be working.

--- zk-init.sh
+++ zk-init.sh
@@ -1,38 +1,24 @@
 #!/bin/bash
 
 ZK=$1
-MYID=1
 IPADDRESS=`ip -4 addr show scope global dev eth0 | grep inet | awk '{print \$2}' | cut -d / -f 1`
+MYID=$(echo $IPADDRESS | sed 's/\.//g')
 
 cd /tmp/zookeeper
 if [ -n "$ZK" ] 
 then
-  output=`./bin/zkCli.sh -server $ZK:2181 get /zookeeper/config | grep ^server` 
-  
-  # extract all the zk-ids from the output
-  declare -a id_list=()
-  while read x;
-  do
-    id_list+=(`echo $x | cut -d"=" -f1 | cut -d"." -f2`)
-  done <<<$(output)
-  sorted_id_list=( $(
-    for el in "${id_list[@]}"
-    do  
-      echo "$el"
-    done | sort -n) )
-  
-  # get the next increasing number from the sequence
-  MYID=$((${sorted_id_list[${#sorted_id_list[@]}-1]}+1))
-  echo $output >> /tmp/zookeeper/conf/zoo.cfg.dynamic
-  echo "server.$MYID=$IPADDRESS:2888:3888:observer;2181" >> /tmp/zookeeper/conf/zoo.cfg.dynamic
-  cp /tmp/zookeeper/conf/zoo.cfg.dynamic /tmp/zookeeper/conf/zoo.cfg.dynamic.org
+  ./bin/zkCli.sh -server $ZK:2181 get /zookeeper/config | grep ^server >> /tmp/zookeeper/conf/zoo.cfg
+  echo "server.$MYID=$IPADDRESS:2888:3888:observer;2181" >> /tmp/zookeeper/conf/zoo.cfg
+  cp /tmp/zookeeper/conf/zoo.cfg /tmp/zookeeper/conf/zoo.cfg.org
   /tmp/zookeeper/bin/zkServer-initialize.sh --force --myid=$MYID
   ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /tmp/zookeeper/bin/zkServer.sh start
+  sleep 10
   /tmp/zookeeper/bin/zkCli.sh -server $ZK:2181 reconfig -add "server.$MYID=$IPADDRESS:2888:3888:participant;2181"
-  /tmp/zookeeper/bin/zkServer.sh stop
-  ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /tmp/zookeeper/bin/zkServer.sh start-foreground
+  while true; do sleep 10000; done # block forever
+  #/tmp/zookeeper/bin/zkServer.sh stop
+  #ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /tmp/zookeeper/bin/zkServer.sh start-foreground
 else
-  echo "server.$MYID=$IPADDRESS:2888:3888;2181" >> /tmp/zookeeper/conf/zoo.cfg.dynamic
+  echo "server.$MYID=$IPADDRESS:2888:3888;2181" >> /tmp/zookeeper/conf/zoo.cfg
   /tmp/zookeeper/bin/zkServer-initialize.sh --force --myid=$MYID
   ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /tmp/zookeeper/bin/zkServer.sh start-foreground
 fi
