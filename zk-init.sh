8f7cf2923a48ccab08abccb561b2d1e441d54bf4
ccrain <charles.crain@kukanao.com>
2015-11-13 22:38:26 -0600

    Cleared up some commented out lines in zk_init

--- zk-init.sh
+++ zk-init.sh
@@ -15,8 +15,6 @@ then
   sleep 10
   /tmp/zookeeper/bin/zkCli.sh -server $ZK:2181 reconfig -add "server.$MYID=$IPADDRESS:2888:3888:participant;2181"
   while true; do sleep 10000; done # block forever
-  #/tmp/zookeeper/bin/zkServer.sh stop
-  #ZOO_LOG_DIR=/var/log ZOO_LOG4J_PROP='INFO,CONSOLE,ROLLINGFILE' /tmp/zookeeper/bin/zkServer.sh start-foreground
 else
   echo "server.$MYID=$IPADDRESS:2888:3888;2181" >> /tmp/zookeeper/conf/zoo.cfg
   /tmp/zookeeper/bin/zkServer-initialize.sh --force --myid=$MYID
